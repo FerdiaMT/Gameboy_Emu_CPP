de119439a6d405054eb40c5874b43f343ab28bfb
FerdiaMT <oscadot@gmail.com>
2025-10-18 14:46:12 +0100

    Splitting main into 2 files, main will just handle io and rendering logic, all actual gameboy logic will be held solely in gameboy.cpp

--- SharpSM83/Gameboy.cpp
+++ SharpSM83/Gameboy.cpp
@@ -1,3 +1,4 @@
+#include "Gameboy.h"
 #include "SM83.h"
 #include "Memory.h"
 #include "PPU.h"
@@ -18,7 +19,6 @@ bool running = true;
 
 SDL_Window* window;
 SDL_Renderer* renderer;
-SDL_Texture* texture;
 
 
 SDL_Window* window2;
@@ -33,12 +33,12 @@ const int SCALE = 5;
 
 
 uint8_t pixelState[160][144]{}; // for now this can be 0,1,2,3,4
-bool keyboardState[6]{true,true,true,true,true,true};
+bool keyboardState[6]{ true,true,true,true,true,true };
 
 /*
 Todo:
 
-    OAM transfer 
+    OAM transfer
     STAT reg
     replace view with read in cpu
 */
@@ -91,16 +91,6 @@ void setupSDL()
         SDL_Quit;
         return;
     }
-
-    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_STREAMING, 160, 144);
-
-    if (!texture)
-    {
-        std::cerr << "texture couldnt be created " << SDL_GetError();
-        SDL_Quit;
-        return;
-    }
-
 }
 
 void setupSDL2()
@@ -111,7 +101,7 @@ void setupSDL2()
         return;
     }
 
-    window2 = SDL_CreateWindow("ABC", 8*16 * SCALE, 8*24 * SCALE, SDL_WINDOW_RESIZABLE);
+    window2 = SDL_CreateWindow("ABC", 8 * 16 * SCALE, 8 * 24 * SCALE, SDL_WINDOW_RESIZABLE);
     if (!window2)
     {
         std::cerr << "Window couldnt be created " << SDL_GetError();
@@ -163,7 +153,7 @@ void drawTextureWindow(Memory& memory) // going to use this to draw out the text
             for (int y = 0; y < 8; y++) {
 
                 uint8_t row1 = memory.readPPU(level + (y * 2) + (tw * 16) + (th * 16 * 8 * 2));
-                uint8_t row2 = memory.readPPU(level + (y * 2) + 1 + (tw * 16) + (th * 16 * 8*2));
+                uint8_t row2 = memory.readPPU(level + (y * 2) + 1 + (tw * 16) + (th * 16 * 8 * 2));
 
                 for (int x = 0; x < 8; x++) {
 
@@ -177,7 +167,7 @@ void drawTextureWindow(Memory& memory) // going to use this to draw out the text
                         c = 255 - (mainBit * 100);
                     }
                     SDL_SetRenderDrawColor(renderer2, c, c, c, 255);
-                    SDL_FRect pixel = { (SCALE * tw*8) + (x * SCALE),  (SCALE * th * 8) + (y * SCALE), SCALE, SCALE};//x,y,w,h
+                    SDL_FRect pixel = { (SCALE * tw * 8) + (x * SCALE),  (SCALE * th * 8) + (y * SCALE), SCALE, SCALE };//x,y,w,h
                     SDL_RenderFillRect(renderer2, &pixel);
                 }
             }
@@ -253,7 +243,7 @@ static void drawMapA(Memory& memory)
             for (int y = 0; y < 8; y++) {
 
                 uint8_t row1 = memory.readPPU(memAdress2 + tileNumber + (y * 2));
-                uint8_t row2 = memory.readPPU(memAdress2 + tileNumber + (y * 2)+1);
+                uint8_t row2 = memory.readPPU(memAdress2 + tileNumber + (y * 2) + 1);
 
                 for (int x = 0; x < 8; x++) {
 
@@ -271,11 +261,11 @@ static void drawMapA(Memory& memory)
                     SDL_FRect pixel2 = { (SCALE * tx * 8) + (x * SCALE),  (SCALE * ty * 8) + (y * SCALE), SCALE, SCALE };//x,y,w,h
                     SDL_RenderFillRect(renderer3, &pixel2);
                 }
-              
+
             }
-           
+
         }
-     
+
     }
     SDL_RenderPresent(renderer3);
 }
@@ -284,14 +274,14 @@ static void drawMapB(Memory& memory)
 {
     uint8_t c = 0;
     uint16_t mapAddr = 0x9800;
-    uint16_t tileDataBase = 0x9000; 
+    uint16_t tileDataBase = 0x9000;
 
     for (int ty = 0; ty < 32; ty++) {
         for (int tx = 0; tx < 32; tx++) {
 
             int8_t tileNumber = (int8_t)memory.readPPU(mapAddr + tx + (ty * 32));
 
-            uint16_t tileAddr = tileDataBase + ( (int16_t)tileNumber * 16);
+            uint16_t tileAddr = tileDataBase + ((int16_t)tileNumber * 16);
 
             for (int y = 0; y < 8; y++) {
 
@@ -330,19 +320,22 @@ double cpuCyclesPerFrame = 4194304.0 / 59.7;
 
 int main()
 {
-    setupSDL();
-    //setupSDL2(); //<- textureviewer
-     //setupSDL3(); // <- map viewer
+    Memory memory;
+    SM83 cpu(memory);
+    PPU ppu(memory);
+    Clock clock(memory);
+    cpu.reset();
+    memory.reset();
+    ppu.resetPPU();
+    clock.resetClock();
 
-    if (!initPixelRenderer(renderer)) {
-        printf("Failed to initialize pixel renderer!\n");
-        return 1;
-    }
-
-    Gameboy gb;
-
+    //CONFIRMED WORKING      : 1,(),3,4,5,6,7 ,8 ,9 ,[10] , 11
+    // LONG / INCOMPLETE     : 11 (200MB+)  
+    // DEBUGS TO GET WORKING: cpu_instrs
     std::ifstream file("02.gb", std::ios::binary | std::ios::ate);
-    bool skipBootROM = true; 
+
+    bool skipBootROM = true;
+
     if (file.is_open())
     {
         std::streampos size = file.tellg();
@@ -351,9 +344,57 @@ int main()
         file.read(buffer, size);
         file.close();
 
-        for (int i = 0; i < size; ++i){ memory.write(i, buffer[i]);}
+        for (int i = 0; i < size; ++i) { memory.write(i, buffer[i]); }
         delete[] buffer;
         std::cout << "done loading memory" << std::endl;
+    }
+
+
+
+    if (skipBootROM) {
+
+        cpu.debugRegs();
+
+        memory.write(0xFF05, 0x00);
+        memory.write(0xFF06, 0x00);
+        memory.write(0xFF07, 0x00);
+        memory.write(0xFF10, 0x80);
+        memory.write(0xFF11, 0xBF);
+        memory.write(0xFF12, 0xF3);
+        memory.write(0xFF14, 0xBF);
+        memory.write(0xFF16, 0x3F);
+        memory.write(0xFF17, 0x00);
+        memory.write(0xFF19, 0xBF);
+        memory.write(0xFF1A, 0x7F);
+        memory.write(0xFF1B, 0xFF);
+        memory.write(0xFF1C, 0x9F);
+        memory.write(0xFF1E, 0xBF);
+        memory.write(0xFF20, 0xFF);
+        memory.write(0xFF21, 0x00);
+        memory.write(0xFF22, 0x00);
+        memory.write(0xFF23, 0xBF);
+        memory.write(0xFF24, 0x77);
+        memory.write(0xFF25, 0xF3);
+        memory.write(0xFF26, 0xF1);
+        memory.write(0xFF40, 0x91);
+        memory.write(0xFF42, 0x00);
+        memory.write(0xFF43, 0x00);
+        memory.write(0xFF45, 0x00);
+        memory.write(0xFF47, 0xFC);
+        memory.write(0xFF48, 0xFF);
+        memory.write(0xFF49, 0xFF);
+        memory.write(0xFF4A, 0x00);
+        memory.write(0xFF4B, 0x00);
+        memory.write(0xFF50, 0x01);
+    }
+    else {
+        for (uint16_t i = 0; i < 256; i++) {
+            memory.write(i, bootRO[i]);
+        }
+        for (uint16_t i = 0x104; i < 0x133; i++)
+        {
+            memory.write(i, headerRO[i - 0x104]);
+        }
     }
 
     int allCycles{};
@@ -361,14 +402,77 @@ int main()
     bool renderFrame = false;
     uint16_t cpuDotsFromExec{};
 
-   
-
+    setupSDL();
+    setupSDL2(); //<- textureviewer
+    //setupSDL3(); // <- map viewer
     SDL_Event event;
 
+    if (!initPixelRenderer(renderer)) {
+        // Handle error - maybe exit the program
+        printf("Failed to initialize pixel renderer!\n");
+        return 1;
+    }
 
-
-    while(running)
+    while (running)
     {
+
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        if (allCycles >= 17573)//dt >= 16.73/*16.73*/) // once every 60 seconds (16.73 milliseconds)
+        {
+            ppu.updateScreenBuffer(pixelState);
+            drawAllPixels(memory);
+
+            float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
+            if (dt < 16.73)
+            {
+                using namespace std::chrono_literals;
+                auto wait = (16.73 - dt) * 1ms;
+                std::this_thread::sleep_for(wait);
+            }
+            lastCycleTime = currentTime;
+            allCycles = 0;
+            cpu.cycles = 0;
+            clock.resetClockCycle();
+
+            drawTextureWindow(memory);
+            //drawMapA(memory);
+
+            //std::cout << "LY=" << std::dec << (int)memory.ioFetchLY()
+            //    << " IF=" << std::hex << (int)memory.ioFetchIF()
+            //    << " IE=" << std::hex << (int)memory.ioFetchIE()
+            //    << std::endl;
+
+        }
+
+        cpuDotsFromExec = cpu.executeInstruction();
+
+        //std::cout << "last cycle was " << std::dec << (int)cpuDotsFromExec << " with OP:  " << std::hex << (int)cpu.getLastOP();if (memory.badWrite){std::cout << "   bad write at LY:" <<std::dec<< (int)memory.ioFetchLY() << "  PC :" << (int)cpu.getPC() << "  PPU CYCLE: " << (int)ppu.getInternalDot();memory.badWrite = false;}std::cout<< std::endl;
+
+        uint16_t c{};
+
+        if ((memory.ioFetchLCDC() & 0x80) != 0) // check lcdc bit 7
+        {
+            for (int i = 0; i < cpuDotsFromExec; i++)
+            {
+                ppu.executeTick();
+            }
+        }
+        else {
+            memory.ioWriteLY(0);
+        }
+
+        if (memory.writeToDiv() == true)
+        {
+            clock.divCounter = 0;
+        }
+        for (int i = 0; i < cpuDotsFromExec; i++)
+        {
+            clock.executeTick();
+        }
+
+        allCycles += cpuDotsFromExec / 4;
+
+
 
         while (SDL_PollEvent(&event))
         {
@@ -400,15 +504,12 @@ int main()
             }
         }
 
-        for(int i = 0; i < 70224; i+= gb.cpu.cycles)
-        {
-            gb.step();
-        }
+        memory.insertKeyboard(keyboardState);
 
     }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
-	return 0;
+    return 0;
 }
